9ea09d6b557a8d2bdff5eb6bf07d0b13d5f07881
jiahaolong <jiahaolong@axzo.cn>
2022-06-09 10:42:01 +0800

    完成了分类页面的开发

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import {
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
 // 配置请求根路径
-$http.baseUrl = 'https://api-ugo-web.itheima.net'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 // 请求开始之前做一些事情
 $http.beforeRequest = function(options) {
 	uni.showLoading({
